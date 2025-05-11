12ea79ea3bbd1682faa56befef481e868a657dfd
Tamya Chanell <tamyachanell@gmail.com>
2025-05-11 18:05:36 -0500

    fixing the images for netlify

--- FrontEnd/html/js/script.js
+++ FrontEnd/html/js/script.js
@@ -62,6 +62,7 @@ fetch("https://sophie-bluel-ea7w.onrender.com/api/categories")
 function insertJobs(jobs) {
   galleryDiv.innerHTML = ""; // Clear the gallery
   jobs.forEach(({ imageUrl, title }) => {
+     const fixedImageUrl = imageUrl.replace("http://localhost:5678", "https://sophie-bluel-ea7w.onrender.com");
     galleryDiv.innerHTML += `
       <figure>
         <img src="${imageUrl}" alt="${title}">
